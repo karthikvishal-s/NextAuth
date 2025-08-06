d9b828c03f317fa0293038272085933e08b61906
Karthik Vishal <karthikvishal1506@gmail.com>
2025-08-06 14:40:36 +0530

    environment variables added

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -16,6 +16,8 @@ export const options = {
                     role: userRole,
                 };
             },
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
         GoogleProvider({
             profile(profile){
@@ -28,5 +30,20 @@ export const options = {
                     role: userRole,
                 };
             },
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
-    ]}
+    ],
+
+    callbacks:{
+        async jwt({token,user}){
+            if(user) token.role = user.role;
+            return token;
+        },
+        async session({session,token}){
+            if(session?.user) session.user.role = token.role;
+            return session;
+        }
+    }
+
+}
