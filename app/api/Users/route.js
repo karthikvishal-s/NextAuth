74a928aaaeef7c1bd5a0ff6a2625343c0e555b9e
Karthik Vishal <karthikvishal1506@gmail.com>
2025-08-06 19:36:12 +0530

    route.js for Users - api backend done..(Validation of user form kind of)

---
+++ app/api/Users/route.js
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import User from "@/app/(models)/User";
+import bcrypt from "bcrypt";
+
+export async function POST(req){
+    try{
+        const body = await req.json();
+        const userData = body.formData
+
+        if(!userData?.email || !userData.password){
+            return NextResponse.json({error: "All Fields are required",err}, {status: 400});
+        }
+
+        // Check for duplicate email
+
+        const duplicate = await User.findOne({email: userData.email})
+        .lean()
+        .exec();
+
+    if(duplicate){
+        return NextResponse.json
+        ({message: "User already exists(Email)"}, {status: 409});
+    }
+
+    const hashPassword = await bcrypt.hash(userData.password, 10);
+    userData.password = hashPassword;
+
+    await User.create(userData);
+    return NextResponse.json({message: "User created successfully"}, {status: 201});
+
+    } catch (error){
+        console.log(err)
+        return NextResponse.json({error: "Error",err}, {status: 500});
+    }
+}
